c458d1c1e4625324f15a7c85b4ebf1bc17c54289
Carl in 't Veld <carl@intveld.nl>
2023-01-23 15:30:02 +0100

    fix

--- Examples/shared-module-configuration-example/Infrastructure/SharedConfigurationModule/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  default = {
-    "key1" = "defaultvalue1"
-    "key2" = "defaultvalue2"
-  }
-}
--- Examples/shared-module-configuration-example/Infrastructure/SharedConfigurationModule/outputs.tf
+++ Examples/shared-module-configuration-example/Infrastructure/SharedConfigurationModule/outputs.tf
@@ -1,6 +1,6 @@
 output "specific_configuration" {
   value = {
-    key1 = try(var.specific_configuration.key1, local.default.key1)
-    key2 = try(var.specific_configuration.key2, local.default.key2)
+    key1 = try(var.specific_configuration.key1, var.shared_configuration.key1)
+    key2 = try(var.specific_configuration.key2, var.shared_configuration.key2)
   }
 }
